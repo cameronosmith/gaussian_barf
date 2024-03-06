03ead466b53dcca4d0ebbea93c8ecd8ef86c5550
David <david.charatan@gmail.com>
2024-03-06 02:21:14 -0500

    actual conda init

--- run_slurm.py
+++ run_slurm.py
@@ -44,8 +44,8 @@ def submit_slurm_job(
 #SBATCH --qos=vision-sitzmann-main
 #SBATCH --time={time}
 
-/scratch/charatan/miniconda3/bin/activate
 source {Path.home()}/.bashrc
+/scratch/charatan/miniconda3/bin/conda init
 cd {job_dir}
 conda activate {env_name}
 
