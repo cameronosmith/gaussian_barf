ee46f7690139790f5035a0ae11a7a193bb068033
David <david.charatan@gmail.com>
2024-03-06 02:35:28 -0500

    Add script to submit jobs.

---
+++ one_offs/run_splats.sh
@@ -0,0 +1,17 @@
+for METHOD in \
+    colmap \
+    flowmap
+do
+    for SCENE in \
+        co3d_bench \
+        co3d_hydrant \
+        llff_flower \
+        llff_horns \
+        mipnerf360_bonsai \
+        mipnerf360_kitchen \
+        tandt_caterpillar \
+        tandt_playground
+    do
+        python3 run_slurm.py python3 train.py -s ~/datasets/flowmap/${METHOD}/${SCENE} --name ${SCENE}_${METHOD} -o
+    done
+done
