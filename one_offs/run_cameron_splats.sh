e6827115d1451159664f107a44e4f2e7fd0e4689
David <david.charatan@gmail.com>
2024-03-06 14:21:09 -0500

    update one-offs

---
+++ one_offs/run_cameron_splats.sh
@@ -0,0 +1,19 @@
+for METHOD in \
+    colmap \
+    flowmap
+do
+    for SCENE in \
+        bench \
+        hydrant \
+        flower \
+        horns \
+        bonsai \
+        kitchen \
+        caterpillar \
+        playground \
+        office0 \
+        room0
+    do
+        python3 run_slurm.py python3 train.py -s /scratch/charatan/flowmap_converted/${METHOD}/${SCENE} --name paper_c1_${SCENE}_${METHOD} -o
+    done
+done
