dd3a3aba522803441842debdcd112ca5ca2efcbf
David <david.charatan@gmail.com>
2024-03-14 00:05:23 -0400

    dry run supplemental videos

---
+++ render_supplemental.sh
@@ -0,0 +1,20 @@
+for METHOD in \
+    flowmap_ablation_depth_var_rerun \
+    flowmap_ablation_focal_var_rerun \
+    flowmap_ablation_implicit_rerun \
+    flowmap_ablation_pose_var_rerun \
+    flowmap_ablation_two_stage_rerun
+do
+    for SCENE in \
+        bench \
+        bonsai \
+        caterpillar \
+        flower \
+        horns \
+        hydrant \
+        kitchen \
+        playground
+    do
+        echo python3 run_slurm.py python3 train.py -s /scratch/charatan/flowmap_rerun_ablations_converted/${METHOD}/${SCENE} --name paper_c2_${SCENE}_${METHOD} --render_checkpoint output/paper_c2_${SCENE}_${METHOD}/point_cloud/iteration_30000/point_cloud.ply
+    done
+done
