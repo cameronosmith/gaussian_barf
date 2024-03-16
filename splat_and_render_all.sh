b7dde60f15adc7c8e7ed855c3a1a0ef2fa416a7f
David <david.charatan@gmail.com>
2024-03-16 16:09:06 -0400

    fix script

--- splat_and_render_all.sh
+++ splat_and_render_all.sh
@@ -10,7 +10,7 @@ for METHOD in \
     ablation_single_stage
 do
     for SCENE in /scratch/charatan/projects/flowmap/results/colmap/*/ ; do
-        python3 run_slurm.py python3 train.py -s /scratch/charatan/projects/flowmap/results/${METHOD}/${SCENE} --name paper_v14_${SCENE}_${METHOD}
-        python3 run_slurm.py python3 train.py -s /scratch/charatan/projects/flowmap/results/${METHOD}/${SCENE} --name paper_v14_${SCENE}_${METHOD} --render_checkpoint output/paper_v14_${SCENE}_${METHOD}/point_cloud/iteration_30000/point_cloud.ply
+        python3 run_slurm.py python3 train.py -s /scratch/charatan/projects/flowmap/results/${METHOD}/$(basename ${SCENE}) --name paper_v14_$(basename ${SCENE})_${METHOD}
+        python3 run_slurm.py python3 train.py -s /scratch/charatan/projects/flowmap/results/${METHOD}/$(basename ${SCENE}) --name paper_v14_$(basename ${SCENE})_${METHOD} --render_checkpoint output/paper_v14_$(basename ${SCENE})_${METHOD}/point_cloud/iteration_30000/point_cloud.ply
     done
 done
