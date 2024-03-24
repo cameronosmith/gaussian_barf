f5c0f23ce2e68aa8ec9d61b3ee85a3fd8dd27614
David <david.charatan@gmail.com>
2024-03-24 17:13:59 -0400

    update splatting script

--- splat.sh
+++ splat.sh
@@ -1,10 +1,9 @@
 for METHOD in \
+    flowmap_ablation_none \
     colmap \
     mvscolmap \
-    droid \
-    flowmap_ablation_none \
     flowmap_ablation_random_initialization \
-    flowmap_ablation_midas_initialization \
+    flowmap_ablation_random_initialization_long \
     flowmap_ablation_explicit_depth \
     flowmap_ablation_explicit_focal_length \
     flowmap_ablation_explicit_pose \
@@ -12,7 +11,7 @@ for METHOD in \
     flowmap_ablation_no_tracks \
     flowmap_ablation_single_stage
 do
-    for SCENE in /scratch/charatan/projects/flowmap/results/colmap/*/ ; do
-        python3 run_slurm.py python3 train.py -s /scratch/charatan/projects/flowmap/results/${METHOD}/$(basename ${SCENE}) --name paper_v14_${METHOD}_$(basename ${SCENE}) -o
+    for SCENE in /scratch/datasets/flowmap/*/ ; do
+        python3 run_slurm.py python3 train.py -s /scratch/charatan/projects/flowmap/results/${METHOD}/$(basename ${SCENE}) --name paper_v17_${METHOD}_$(basename ${SCENE}) -o
     done
 done
