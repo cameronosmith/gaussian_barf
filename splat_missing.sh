fcbab7c9cd132793b18c9b7278fb3c9281581611
David Charatan <david.charatan@gmail.com>
2024-04-04 14:26:32 -0400

    add script to splat missing runs

---
+++ splat_missing.sh
@@ -0,0 +1,7 @@
+python3 run_slurm.py python3 train.py -s /scratch/charatan/projects/flowmap/results/flowmap_ablation_none/llff_fortress --name paper_v17_flowmap_ablation_none_llff_fortress -o
+python3 run_slurm.py python3 train.py -s /scratch/charatan/projects/flowmap/results/flowmap_ablation_none/llff_leaves --name paper_v17_flowmap_ablation_none_llff_leaves -o
+python3 run_slurm.py python3 train.py -s /scratch/charatan/projects/flowmap/results/flowmap_ablation_none/llff_horns --name paper_v17_flowmap_ablation_none_llff_horns -o
+python3 run_slurm.py python3 train.py -s /scratch/charatan/projects/flowmap/results/flowmap_ablation_none/llff_flower --name paper_v17_flowmap_ablation_none_llff_flower -o
+python3 run_slurm.py python3 train.py -s /scratch/charatan/projects/flowmap/results/flowmap_ablation_none/llff_orchids --name paper_v17_flowmap_ablation_none_llff_orchids -o
+python3 run_slurm.py python3 train.py -s /scratch/charatan/projects/flowmap/results/flowmap_ablation_none/llff_trex --name paper_v17_flowmap_ablation_none_llff_trex -o
+python3 run_slurm.py python3 train.py -s /scratch/charatan/projects/flowmap/results/flowmap_ablation_no_correspondence_weights/llff_trex --name paper_v17_flowmap_ablation_no_correspondence_weights_llff_trex -o
